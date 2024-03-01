30ed60d121cc401d0ff7d497b1d293c2bc6f6254
mojukali <mohamadmossaloji15@gmail.com>
2024-03-01 10:44:19 +0700

    Third Commit

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,9 +7,9 @@
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-   <link rel="stylesheet" href="storage/css/style.css">
-   <link rel="stylesheet" href="../storage/css/style.css">
-   <link rel="stylesheet" href="/storage/css/style.css">
+   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="../css/style.css">
+   <link rel="stylesheet" href="/css/style.css">
 </head>
 
 <body>
